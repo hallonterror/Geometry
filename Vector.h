46d0f7d67b928168428861b644bbf2bf08699404
hallonterror <daniel@rydstroms.com>
2016-11-28 23:59:04 +0100

    Adding operator overloading
    Changed the name of Vec to Vector. Removed Point as there is for now no
    specific use case for this type.
    Some operators are still not working (minus is incorrect).
    Most of the boiler plate is there though.
    Next is to add MatrixOperations.

---
+++ Vector.h
@@ -0,0 +1,167 @@
+#pragma once
+
+#include "Types.h"
+
+namespace dry
+{
+  template <typename T>
+  class Vector2
+  {
+  public:
+    Vector2() {};
+    Vector2(const T& x, const T& y) : x(x), y(y) {}
+
+    T x;
+    T y;
+
+    T& operator[](size_t idx) { return *(&x+idx); }
+    const T& operator[](size_t idx) const { return *(&x + idx); }
+
+    template <typename U>
+    Vector2 operator*=(U f)
+    {
+      x *= f;
+      y *= f;
+      return *this;
+    }
+    template <typename U>
+    Vector2 operator/=(U f)
+    {
+      x /= f;
+      y /= f;
+      return *this;
+    }
+    template <typename U>
+    Vector2 operator+=(U f)
+    {
+      x += f;
+      y += f;
+      return *this;
+    }
+    template <typename U>
+    Vector2<T> operator-=(U f)
+    {
+      x -= f;
+      y -= f;
+      return *this;
+    }
+
+    T norm() { return std::sqrt(vec.x*vec.x + vec.y*vec.y); }
+    T norm2() { return vec.x*vec.x + vec.y*vec.y; }
+  };
+
+  template <typename T>
+  class Vector3
+  {
+  public:
+    Vector3() {};
+    Vector3(const T& x, const T& y, const T& z) : x(x), y(y), z(z) {}
+
+    T x;
+    T y;
+    T z;
+
+    T& operator[](size_t idx) { return *(&x + idx); }
+    const T& operator[](size_t idx) const { return *(&x + idx); }
+
+    template <typename U>
+    Vector3 operator*=(U f)
+    {
+      x *= f;
+      y *= f;
+      z *= f;
+      return *this;
+    }
+    template <typename U>
+    Vector3 operator/=(U f)
+    {
+      x /= f;
+      y /= f;
+      z /= f;
+      return *this;
+    }
+    template <typename U>
+    Vector3 operator+=(U f)
+    {
+      x += f;
+      y += f;
+      z += f;
+      return *this;
+    }
+    template <typename U>
+    Vector3 operator-=(U f)
+    {
+      x -= f;
+      y -= f;
+      z -= f;
+      return *this;
+    }
+
+    T norm() { return std::sqrt(vec.x*vec.x + vec.y*vec.y + vec.z*vec.z); }
+    T norm2() { return vec.x*vec.x + vec.y*vec.y + vec.z*vec.z; }
+  };
+
+  template <typename T>
+  class Vector4
+  {
+  public:
+    Vector4() {};
+    Vector4(const T& x, const T& y, const T& z, const T& w) : x(x), y(y), z(z), w(w) {}
+
+    T x;
+    T y;
+    T z;
+    T w;
+
+    T& operator[](size_t idx) { return *(&x + idx); }
+    const T& operator[](size_t idx) const { return *(&x + idx); }
+
+    template <typename U>
+    Vector4 operator*=(U f)
+    {
+      x *= f;
+      y *= f;
+      z *= f;
+      w *= f;
+      return *this;
+    }
+    template <typename U>
+    Vector4 operator/=(U f)
+    {
+      x /= f;
+      y /= f;
+      z /= f;
+      w *= f;
+      return *this;
+    }
+    template <typename U>
+    Vector4 operator+=(U f)
+    {
+      x += f;
+      y += f;
+      z += f;
+      w *= f;
+      return *this;
+    }
+    template <typename U>
+    Vector4 operator-=(U f)
+    {
+      x -= f;
+      y -= f;
+      z -= f;
+      w *= f;
+      return *this;
+    }
+
+    T norm() { return std::sqrt(vec.x*vec.x + vec.y*vec.y + vec.z*vec.z + vec.w*vec.w); }
+    T norm2() { return vec.x*vec.x + vec.y*vec.y + vec.z*vec.z + vec.w*vec.w; }
+  };
+
+  typedef Vector2<float32> Vector2f;
+  typedef Vector3<float32> Vector3f;
+  typedef Vector4<float32> Vector4f;
+
+  typedef Vector2<float64> Vector2d;
+  typedef Vector3<float64> Vector3d;
+  typedef Vector4<float64> Vector4d;
+}
---
+++ VectorOperations.h
@@ -0,0 +1,188 @@
+#pragma once
+
+#include "Vector.h"
+
+namespace dry
+{
+  template <typename T>
+  Vector2<T> normalized(const Vector2<T>& vec)
+  {
+    T denominator = vec.norm();
+    return Vector2<T>(vec.x / denominator, vec.y / denominator);
+  }
+  template <typename T>
+  Vector3<T> normalized(const Vector3<T>& vec)
+  {
+    T denominator = vec.norm();
+    return Vector3<T>(vec.x / denominator, vec.y / denominator, vec.z / denominator);
+  }
+  template <typename T>
+  Vector4<T> normalized(const Vector4<T>& vec)
+  {
+    T denominator = vec.norm();
+    return Vector4<T>(vec.x / denominator, vec.y / denominator, vec.z / denominator, vec.w / denominator);
+  }
+
+  template <typename T>
+  void normalize(Vector2<T>& vec)
+  {
+    T denominator = vec.norm();
+    vec.x = vec.x / denominator;
+    vec.y = vec.y / denominator;
+  }
+  template <typename T>
+  void normalize(Vector3<T>& vec)
+  {
+    T denominator = vec.norm();
+    vec.x = vec.x / denominator;
+    vec.y = vec.y / denominator;
+    vec.z = vec.z / denominator;
+  }
+  template <typename T>
+  void normalize(Vector4<T>& vec)
+  {
+    T denominator = vec.norm();
+    vec.x = vec.x / denominator;
+    vec.y = vec.y / denominator;
+    vec.z = vec.z / denominator;
+    vec.w = vec.w / denominator;
+  }
+
+  template <typename T>
+  T dot(const Vector2<T>& first, const Vector2<T>& second)
+  {
+    return first.x*second.x + first.y*second.y;
+  }
+  template <typename T>
+  T dot(const Vector3<T>& first, const Vector3<T>& second)
+  {
+    return first.x*second.x + first.y*second.y + first.z*second.z;
+  }
+  template <typename T>
+  T dot(const Vector4<T>& first, const Vector4<T>& second)
+  {
+    return first.x*second.x + first.y*second.y + first.z*second.z + first.w*second.w;
+  }
+
+  template <typename T>
+  Vector3<T> cross(const Vector3<T>& first, const Vector3<T>& second)
+  {
+    return Vector3<T>(
+      first.y*second.z - first.z*second.y,
+      first.z*second.x - first.x*second.z, 
+      first.x*second.y - first.y*second.x);
+  }
+
+  template <typename T, typename U>
+  inline Vector2<T> operator* (Vector2<T> vec, U f) {
+    vec *= f;
+    return vec;
+  }
+  template <typename T, typename U>
+  inline Vector2<T> operator* (U f, const Vector2<T>& vec) {
+    return vec * f;
+  }
+  template <typename T, typename U>
+  inline Vector2<T> operator/ (Vector2<T> vec, U f) {
+    vec /= f;
+    return vec;
+  }
+  template <typename T, typename U>
+  inline Vector2<T> operator/ (U f, const Vector2<T>& vec) {
+    return vec / f;
+  }
+  template <typename T, typename U>
+  inline Vector2<T> operator+ (Vector2<T> vec, U f) {
+    vec += f;
+    return vec;
+  }
+  template <typename T, typename U>
+  inline Vector2<T> operator+ (U f, const Vector2<T>& vec) {
+    return vec + f;
+  }
+  template <typename T, typename U>
+  inline Vector2<T> operator- (Vector2<T> vec, U f) {
+    vec -= f;
+    return vec;
+  }
+  template <typename T, typename U>
+  inline Vector2<T> operator- (U f, const Vector2<T>& vec) {
+    return vec - f;
+  }
+
+  template <typename T, typename U>
+  inline Vector3<T> operator* (Vector3<T> vec, U f) {
+    vec *= f;
+    return vec;
+  }
+  template <typename T, typename U>
+  inline Vector3<T> operator* (U f, const Vector3<T>& vec) {
+    return vec * f;
+  }
+  template <typename T, typename U>
+  inline Vector3<T> operator/ (Vector3<T> vec, U f) {
+    vec /= f;
+    return vec;
+  }
+  template <typename T, typename U>
+  inline Vector3<T> operator/ (U f, const Vector3<T>& vec) {
+    return vec / f;
+  }
+  template <typename T, typename U>
+  inline Vector3<T> operator+ (Vector3<T> vec, U f) {
+    vec += f;
+    return vec;
+  }
+  template <typename T, typename U>
+  inline Vector3<T> operator+ (U f, const Vector3<T>& vec) {
+    return vec + f;
+  }
+  template <typename T, typename U>
+  inline Vector3<T> operator- (Vector3<T> vec, U f) {
+    vec -= f;
+    return vec;
+  }
+
+  // TODO: FIX BUG HERE!
+  template <typename T, typename U>
+  inline Vector3<T> operator- (U f, const Vector3<T>& vec) {
+    return vec - f;
+  }
+
+  template <typename T, typename U>
+  inline Vector4<T> operator* (Vector4<T> vec, U f) {
+    vec *= f;
+    return vec;
+  }
+  template <typename T, typename U>
+  inline Vector4<T> operator* (U f, const Vector4<T>& vec) {
+    return vec * f;
+  }
+  template <typename T, typename U>
+  inline Vector4<T> operator/ (Vector4<T> vec, U f) {
+    vec /= f;
+    return vec;
+  }
+  template <typename T, typename U>
+  inline Vector4<T> operator/ (U f, const Vector4<T>& vec) {
+    return vec / f;
+  }
+  template <typename T, typename U>
+  inline Vector4<T> operator+ (Vector4<T> vec, U f) {
+    vec += f;
+    return vec;
+  }
+  template <typename T, typename U>
+  inline Vector4<T> operator+ (U f, const Vector4<T>& vec) {
+    return vec + f;
+  }
+  template <typename T, typename U>
+  inline Vector4<T> operator- (Vector4<T> vec, U f) {
+    vec -= f;
+    return vec;
+  }
+  template <typename T, typename U>
+  inline Vector4<T> operator- (U f, const Vector4<T>& vec) {
+    return vec - f;
+  }
+}
