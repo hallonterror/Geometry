8318a038307b3f7841edcffde14bc9830c4bc3b7
hallonterror <daniel@rydstroms.com>
2016-11-30 22:57:02 +0100

    Updating VectorOperations
    Inlining some functions.
    Adding specializations for Vector [operator] vector.
    Fixing issues with division and minus

--- VectorOperations.h
+++ VectorOperations.h
@@ -5,33 +5,33 @@
 namespace dry
 {
   template <typename T>
-  Vector2<T> normalized(const Vector2<T>& vec)
+  inline Vector2<T> normalized(const Vector2<T>& vec)
   {
     T denominator = vec.norm();
     return Vector2<T>(vec.x / denominator, vec.y / denominator);
   }
   template <typename T>
-  Vector3<T> normalized(const Vector3<T>& vec)
+  inline Vector3<T> normalized(const Vector3<T>& vec)
   {
     T denominator = vec.norm();
     return Vector3<T>(vec.x / denominator, vec.y / denominator, vec.z / denominator);
   }
   template <typename T>
-  Vector4<T> normalized(const Vector4<T>& vec)
+  inline Vector4<T> normalized(const Vector4<T>& vec)
   {
     T denominator = vec.norm();
     return Vector4<T>(vec.x / denominator, vec.y / denominator, vec.z / denominator, vec.w / denominator);
   }
 
   template <typename T>
-  void normalize(Vector2<T>& vec)
+  inline void normalize(Vector2<T>& vec)
   {
     T denominator = vec.norm();
     vec.x = vec.x / denominator;
     vec.y = vec.y / denominator;
   }
   template <typename T>
-  void normalize(Vector3<T>& vec)
+  inline void normalize(Vector3<T>& vec)
   {
     T denominator = vec.norm();
     vec.x = vec.x / denominator;
@@ -39,7 +39,7 @@ namespace dry
     vec.z = vec.z / denominator;
   }
   template <typename T>
-  void normalize(Vector4<T>& vec)
+  inline void normalize(Vector4<T>& vec)
   {
     T denominator = vec.norm();
     vec.x = vec.x / denominator;
@@ -49,28 +49,49 @@ namespace dry
   }
 
   template <typename T>
-  T dot(const Vector2<T>& first, const Vector2<T>& second)
+  inline T dot(const Vector2<T>& first, const Vector2<T>& second)
   {
     return first.x*second.x + first.y*second.y;
   }
   template <typename T>
-  T dot(const Vector3<T>& first, const Vector3<T>& second)
+  inline T dot(const Vector3<T>& first, const Vector3<T>& second)
   {
     return first.x*second.x + first.y*second.y + first.z*second.z;
   }
   template <typename T>
-  T dot(const Vector4<T>& first, const Vector4<T>& second)
+  inline T dot(const Vector4<T>& first, const Vector4<T>& second)
   {
     return first.x*second.x + first.y*second.y + first.z*second.z + first.w*second.w;
   }
 
   template <typename T>
-  Vector3<T> cross(const Vector3<T>& first, const Vector3<T>& second)
+  inline Vector3<T> cross(const Vector3<T>& first, const Vector3<T>& second)
   {
     return Vector3<T>(
       first.y*second.z - first.z*second.y,
       first.z*second.x - first.x*second.z, 
       first.x*second.y - first.y*second.x);
+  }
+
+  template <typename T>
+  inline Vector2<T> toInhomogeneous(const Vector3<T>& vec)
+  {
+    return Vector2<T>(vec.x / vec.z, vec.y / vec.z);
+  }
+  template <typename T>
+  inline Vector3<T> toInhomogeneous(const Vector4<T>& vec)
+  {
+    return Vector3<T>(vec.x / vec.w, vec.y / vec.w, vec.z / vec.w);
+  }
+  template <typename T>
+  inline Vector3<T> toHomogeneous(const Vector2<T>& vec)
+  {
+    return Vector3<T>(vec.x, vec.y, T(1));
+  }
+  template <typename T>
+  inline Vector4<T> toHomogeneous(const Vector3<T>& vec)
+  {
+    return Vector4<T>(vec.x, vec.y, vec.z, T(1.0));
   }
 
   template <typename T, typename U>
@@ -111,6 +132,23 @@ namespace dry
   }
 
   template <typename T, typename U>
+  inline Vector2<T> operator* (const Vector2<T>& vec1, const Vector2<U>& vec2) {
+    return Vector2<T>(vec1.x*vec2.x, vec1.y*vec2.y);
+  }
+  template <typename T, typename U>
+  inline Vector2<T> operator/ (const Vector2<T>& vec1, const Vector2<U>& vec2) {
+    return Vector2<T>(vec1.x/vec2.x, vec1.y/vec2.y);
+  }
+  template <typename T, typename U>
+  inline Vector2<T> operator+ (const Vector2<T>& vec1, const Vector2<U>& vec2) {
+    return Vector2<T>(vec1.x + vec2.x, vec1.y + vec2.y);
+  }
+  template <typename T, typename U>
+  inline Vector2<T> operator- (const Vector2<T>& vec1, const Vector2<U>& vec2) {
+    return Vector2<T>(vec1.x - vec2.x, vec1.y - vec2.y);
+  }
+
+  template <typename T, typename U>
   inline Vector3<T> operator* (Vector3<T> vec, U f) {
     vec *= f;
     return vec;
@@ -145,6 +183,23 @@ namespace dry
   template <typename T, typename U>
   inline Vector3<T> operator- (U f, const Vector3<T>& vec) {
     return Vector3<T>(f - vec.x, f - vec.y, f - vec.z);
+  }
+
+  template <typename T, typename U>
+  inline Vector3<T> operator* (const Vector3<T>& vec1, const Vector3<U>& vec2) {
+    return Vector3<T>(vec1.x*vec2.x, vec1.y*vec2.y, vec1.z*vec2.z);
+  }
+  template <typename T, typename U>
+  inline Vector3<T> operator/ (const Vector3<T>& vec1, const Vector3<U>& vec2) {
+    return Vector3<T>(vec1.x / vec2.x, vec1.y / vec2.y, vec1.z / vec2.z);
+  }
+  template <typename T, typename U>
+  inline Vector3<T> operator+ (const Vector3<T>& vec1, const Vector3<U>& vec2) {
+    return Vector3<T>(vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z);
+  }
+  template <typename T, typename U>
+  inline Vector3<T> operator- (const Vector3<T>& vec1, const Vector3<U>& vec2) {
+    return Vector3<T>(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z);
   }
 
   template <typename T, typename U>
@@ -183,4 +238,22 @@ namespace dry
   inline Vector4<T> operator- (U f, const Vector4<T>& vec) {
     return Vector4<T>(f - vec.x, f - vec.y, f - vec.z, f - vec.w);
   }
+
+  template <typename T, typename U>
+  inline Vector4<T> operator* (const Vector4<T>& vec1, const Vector4<U>& vec2) {
+    return Vector4<T>(vec1.x*vec2.x, vec1.y*vec2.y, vec1.z*vec2.z, vec1.w*vec2.w);
+  }
+  template <typename T, typename U>
+  inline Vector4<T> operator/ (const Vector4<T>& vec1, const Vector4<U>& vec2) {
+    return Vector4<T>(vec1.x / vec2.x, vec1.y / vec2.y, vec1.z / vec2.z, vec1.w / vec2.w);
+  }
+  template <typename T, typename U>
+  inline Vector4<T> operator+ (const Vector4<T>& vec1, const Vector4<U>& vec2) {
+    return Vector4<T>(vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z, vec1.w + vec2.w);
+  }
+  template <typename T, typename U>
+  inline Vector4<T> operator- (const Vector4<T>& vec1, const Vector4<U>& vec2) {
+    return Vector4<T>(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z, vec1.w - vec2.w);
+  }
+
 }
